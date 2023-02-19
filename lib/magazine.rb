e92a8da33ce2dd4f2b2c16379aadfcc6d7ea1126
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-19 14:16:48 +0300

    Primary file for Magazine class

---
+++ lib/magazine.rb
@@ -0,0 +1,16 @@
+class Magazine
+    attr_accessor :name, :category
+
+    @@all = []
+
+    def initialize(name, category)
+        @name = name
+        @category = category
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
+
