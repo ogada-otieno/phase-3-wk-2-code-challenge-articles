b9cfaf2a7ee8e4f7d1a91c18e233fe0f8edce863
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-19 15:15:06 +0300

    array of instances the author has written

--- lib/author.rb
+++ lib/author.rb
@@ -4,5 +4,14 @@ class Author
     def initialize(name)
         @name = name
     end
+
+    # Returns an array of Article instances the author has written
+    def articles
+        Article.all.select do |article|
+            article.author == self
+        end
+    end
+
+   
 end
 
