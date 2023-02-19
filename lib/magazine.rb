581e3a60ae1d60607272bb87d553688e61fecbcb
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-19 15:40:40 +0300

    Returns an array of Author instances who have written for this magazine

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -12,5 +12,18 @@ class Magazine
     def self.all
         @@all
     end
+
+    def articles
+        Article.all {|article| article.magazine == self}
+    end
+
+    def authors
+        articles.collect {|article| article.author}.uniq
+    end
+
+    # Returns an array of Author instances who have written for this magazine
+    def contributors
+        Article.all.filter { |article| article.magazine == self }.map { |article| article.author.name }.uniq
+    end
 end
 
